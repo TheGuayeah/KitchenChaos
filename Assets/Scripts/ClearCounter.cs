f8876b58ff87a17b9720407efc90ccd0f876d489
TheGuayeah <irofer96@gmail.com>
2024-06-27 21:25:21 +0200

    Kitchen Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
-
+    [SerializeField]
+    private KitchenObjectSO kitchenObjectSO;
+    [SerializeField]
+    private Transform counterTopPoint;
 
     public void Interact()
     {
         Debug.Log("Interacted with ClearCounter");
+        Transform kitchenObjectTransform = 
+            Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+
+        if (counterTopPoint.childCount < 0)
+        {
+            kitchenObjectTransform.localPosition = Vector3.zero;
+        }
     }
 }
