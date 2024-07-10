d7be0a523a0d9552b1fec415f9de74eef51bffe0
TheGuayeah <irofer96@gmail.com>
2024-07-10 16:17:10 +0200

    ⚙️ Options: Key Rebinding

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -4,16 +4,49 @@ using UnityEngine.UI;
 
 public class OptionsUI : Singleton<OptionsUI>
 {
+   [Header("Sliders")]
    [SerializeField]
    private Slider sfxSlider;
    [SerializeField]
    private Slider musicSlider;
-   [SerializeField]
-   private Button closeButton;
+
+   [Header("Texts")]
    [SerializeField]
    private TextMeshProUGUI sfxText;
    [SerializeField]
    private TextMeshProUGUI musicText;
+   [SerializeField]
+   private TextMeshProUGUI moveUpText;
+   [SerializeField]
+   private TextMeshProUGUI moveDownText;
+   [SerializeField]
+   private TextMeshProUGUI moveLeftText;
+   [SerializeField]
+   private TextMeshProUGUI moveRightText;
+   [SerializeField]
+   private TextMeshProUGUI interactText;
+   [SerializeField]
+   private TextMeshProUGUI interactAltText;
+   [SerializeField]
+   private TextMeshProUGUI pauseText;
+
+   [Header("Buttons")]
+   [SerializeField]
+   private Button closeButton;
+   [SerializeField]
+   private Button moveUpButton;
+   [SerializeField]
+   private Button moveDownButton;
+   [SerializeField]
+   private Button moveLeftButton;
+   [SerializeField]
+   private Button moveRightButton;
+   [SerializeField]
+   private Button interactButton;
+   [SerializeField]
+   private Button interactAltButton;
+   [SerializeField]
+   private Button pauseButton;
 
    private new void Awake()
    {
@@ -39,6 +72,41 @@ public class OptionsUI : Singleton<OptionsUI>
          GamePauseUI.Instance.Show();
          Hide();
       });
+
+      moveUpButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Move_Up, UpdateVisual);
+      });
+
+      moveDownButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Move_Down, UpdateVisual);
+      });
+
+      moveLeftButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Move_Left, UpdateVisual);
+      });
+
+      moveRightButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Move_Right, UpdateVisual);
+      });
+
+      interactButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Interact, UpdateVisual);
+      });
+
+      interactAltButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Interact_Alternate, UpdateVisual);
+      });
+
+      pauseButton.onClick.AddListener(() =>
+      {
+         GameInput.Instance.RebindBinding(GameInput.Binding.Pause, UpdateVisual);
+      });
    }
 
    private void Start()
@@ -59,6 +127,14 @@ public class OptionsUI : Singleton<OptionsUI>
          Mathf.Round(SoundManager.Instance.GetVolume() * 100f);
       musicText.text = "Music: " +
          Mathf.Round(MusicManager.Instance.GetVolume() * 100f);
+
+      moveUpText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Up);
+      moveDownText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Down);
+      moveLeftText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Left);
+      moveRightText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Move_Right);
+      interactText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact);
+      interactAltText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Interact_Alternate);
+      pauseText.text = GameInput.Instance.GetBindingText(GameInput.Binding.Pause);
    }
 
    public void Show()
